580ba2db1f99ccf12ac2aaf59013c5717172c008
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-07 22:22:23 -0500

    added the code structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react"
+import data from "./data"
+
 function App() {
-  
-  
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log("You Submitted Me!")
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of Boring Lorem Ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={e => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit
+          suscipit explicabo ut repudiandae obcaecati laboriosam cumque non
+          repellat sapiente omnis dolorum quo debitis voluptates illo, nulla
+          nesciunt saepe minus et earum, numquam commodi officia. Magni in
+          repellendus odit eligendi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit
+          suscipit explicabo ut repudiandae obcaecati laboriosam cumque non
+          repellat sapiente omnis dolorum quo debitis voluptates illo, nulla
+          nesciunt saepe minus et earum, numquam commodi officia. Magni in
+          repellendus odit eligendi.
+        </p>
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
